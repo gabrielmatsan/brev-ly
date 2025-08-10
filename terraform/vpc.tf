5de2922471710a6b03c29eafed35c403d668710f
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-08-10 08:49:24 -0300

    feat: adiciona configuração do Terraform para provisionamento de VPC com subnets públicas e privadas, NAT gateway e tags de ambiente

---
+++ terraform/vpc.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+
+  name = "${var.application_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["${var.aws_region}1", "${var.aws_region}2"] # 2 AZs na região (1a e 2a)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"] # 2 subnets privadas
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"] # 2 subnets públicas
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Environment = var.environment
+    Name        = "${var.application_name}-vpc"
+    IAC         = "true"
+  }
+}
