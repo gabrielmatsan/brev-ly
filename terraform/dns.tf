a3022270b9c2e690676aefc41ad5cc172128bc91
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-08-12 19:42:02 -0300

    feat: atualiza registro DNS no Terraform para a API e adiciona estrutura para registro 'www'

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,10 +1,21 @@
 resource "cloudflare_dns_record" "api-dns-record" {
   zone_id = var.cloudflare_zone_id
-  name = var.api_domain_name
+  name    = "api"
   content = aws_lb.alb.dns_name
-  type = "CNAME"
-  ttl = 60
+  type    = "CNAME"
+  ttl     = 60
   proxied = false
 
   depends_on = [aws_lb_listener.alb-listener-https]
 }
+
+# resource "cloudflare_dns_record" "www" {
+#   zone_id = var.cloudflare_zone_id
+#   name = "www"
+#   #content = aws_lb.alb.dns_name colocar o frontend-> cloudfront ou amplify
+#   type = "CNAME"
+#   ttl = 60
+#   proxied = false
+
+#   depends_on = [aws_lb_listener.alb-listener-https]
+# }
