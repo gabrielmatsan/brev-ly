9e4f9f6c411317e600f3e16eda27f98b4b537ce0
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-08-29 16:21:02 -0300

    feat: atualiza registros DNS no Terraform para incluir configuração do frontend e redirecionamento do domínio 'www' para CloudFront

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,21 +1,34 @@
 resource "cloudflare_dns_record" "api-dns-record" {
+  zone_id = var.cloudflare_zone_id # zone_id do cloudflare
+  name    = "api"                  # nome do registro
+  content = aws_lb.alb.dns_name    # dns_name do alb
+  type    = "CNAME"                # tipo de registro
+  ttl     = 60                     # ttl do registro
+  proxied = false                  # se o registro é proxyado pelo cloudflare
+
+  depends_on = [aws_lb_listener.alb-listener-https] # depende do alb-listener-https
+}
+
+# Registro DNS para o frontend via CloudFront (usando CNAME flattening do Cloudflare)
+resource "cloudflare_dns_record" "frontend_dns_record" {
   zone_id = var.cloudflare_zone_id
-  name    = "api"
-  content = aws_lb.alb.dns_name
+  name    = "brev-ly.uk"  # Domínio raiz
+  content = module.cloudfront_distribution.cloudfront_distribution_domain_name
+  type    = "CNAME"
+  ttl     = 1  # TTL automático do Cloudflare
+  proxied = true  # Habilita o proxy do Cloudflare para CNAME flattening
+
+  depends_on = [module.cloudfront_distribution]
+}
+
+# Registro DNS para www redirecionando para o CloudFront
+resource "cloudflare_dns_record" "www_dns_record" {
+  zone_id = var.cloudflare_zone_id
+  name    = "www"
+  content = module.cloudfront_distribution.cloudfront_distribution_domain_name
   type    = "CNAME"
   ttl     = 60
   proxied = false
 
-  depends_on = [aws_lb_listener.alb-listener-https]
+  depends_on = [module.cloudfront_distribution]
 }
-
-# resource "cloudflare_dns_record" "www" {
-#   zone_id = var.cloudflare_zone_id
-#   name = "www"
-#   #content = aws_lb.alb.dns_name colocar o frontend-> cloudfront ou amplify
-#   type = "CNAME"
-#   ttl = 60
-#   proxied = false
-
-#   depends_on = [aws_lb_listener.alb-listener-https]
-# }
