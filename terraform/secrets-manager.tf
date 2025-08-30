f3d41eeba8b91ffc47e63387fa4e29540073c330
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-08-30 01:23:57 -0300

    atualiza chave de credenciais no Secrets Manager, alterando 'token' para 'password'

--- terraform/secrets-manager.tf
+++ terraform/secrets-manager.tf
@@ -13,6 +13,6 @@ resource "aws_secretsmanager_secret_version" "github_credentials_version" {
   secret_id = aws_secretsmanager_secret.github_credentials.id
   secret_string = jsonencode({
     username = var.github_username
-    token    = var.github_token
+    password = var.github_token
   })
 }
