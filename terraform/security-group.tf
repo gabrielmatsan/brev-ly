3a7584bb5f56be43a9cc0c30cb548a58465ddd5d
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-09-02 08:22:49 -0300

    atualiza a configuração de ingress no grupo de segurança do Terraform para permitir acesso a partir do grupo de segurança do ALB, melhorando a segurança e a gestão de tráfego

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -42,10 +42,10 @@ resource "aws_security_group" "ecs" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
   }
 
  // ingress {
